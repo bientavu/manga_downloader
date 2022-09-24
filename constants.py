007d01daf3d907e9611b2a0d1f2b2f72c92ac84a
Axel Mesnard <amesnard@ippon.fr>
2022-09-24 11:41:27 +0200

    Merging + compressing images feat working

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 URL = "https://www.pantheon-scan.fr/overgeared-chapitre-"
-CHAPTER_NUMBERS = list(range(15))
-PARENT_DIR = '/Users/axel/Documents/perso/repos/manga_downloader/chapters'
+CHAPTER_NUMBERS = list(range(122))
+PARENT_DIR = '/Users/axel/Documents/perso/repos/manga_downloader/chapters/'
