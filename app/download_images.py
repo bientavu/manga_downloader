329dee982e7d6969f7a2a32a0d49e169e4bea838
bientavu <dofus.jane@gmail.com>
2023-01-18 17:04:46 +0100

    Feat: better printing for download_images

--- app/download_images.py
+++ app/download_images.py
@@ -12,15 +12,20 @@ from constants import PARENT_DIR
 def get_chapters_urls(chapter_numbers):
     chapter_numbers = chapter_numbers
     list_of_urls = []
+    print("URLs that will be parsed:")
     for chapter_number in chapter_numbers:
         urls = f'{URL}{chapter_number}'
         list_of_urls.append(urls)
-    pprint(list_of_urls)
+        print(urls)
     return list_of_urls
 
 
 def create_folders(chapter_numbers):
-    os.mkdir(PARENT_DIR)
+    if os.path.isdir(PARENT_DIR):
+        print("Chapters folder already created. Skipping... \n")
+    else:
+        os.mkdir(PARENT_DIR)
+        print("Chapters folder created. \n")
     directories = list(range(len(chapter_numbers)))
     for directory in directories:
         try:
@@ -35,11 +40,12 @@ def curl_images_from_urls(urls, full_paths):
     for base, dirs, files in os.walk(PARENT_DIR):
         for _ in dirs:
             number_of_dirs += 1
-    print(f"Number of chapter folders created: {number_of_dirs}")
+    print(f"\n")
+    print(f"Number of chapter folders created: {number_of_dirs} \n")
     list_urls = list(urls)
     for path in full_paths:
         index_path = full_paths.index(path)
-        print(f"Dowloading images for chapter n°{index_path}...")
+        print(f"### Downloading images for chapter n°{index_path}...")
         for url in list_urls:
             page = urllib.request.urlopen(url)
             soup = BSHTML(page, 'html.parser')
@@ -53,7 +59,8 @@ def curl_images_from_urls(urls, full_paths):
                 if image['class'][0] == 'alignnone':
                     fullfilename = os.path.join(f"{path}/", f"{str(index_path).zfill(3)}.jpg")
                     urllib.request.urlretrieve(image['src'], fullfilename)
-                    print(f"{image['src'].rsplit('/', 1)[-1]} downloaded.")
+                    print(f"{image['src'].rsplit('/', 1)[-1]} downloaded and renamed to {fullfilename.rsplit('/', 1)[-1]}")
+            print("\n")
             list_urls.remove(url)
             break
 
