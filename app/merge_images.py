15f0658ef5fff3f91876f4639d0df8c3d3b334f6
bientavu <dofus.jane@gmail.com>
2023-01-18 17:03:40 +0100

    Merging + compressing images feat working

--- app/merge_images.py
+++ app/merge_images.py
@@ -1,57 +1,58 @@
-import itertools
 import os
-import subprocess
-from os import listdir
-from os.path import isfile, join
+
+from constants import PARENT_DIR
 from pathlib import Path
-from pprint import pprint
-import ctypes
-from wand.image import Image
-from wand.api import library
+from PIL import Image
 
 
 def create_folders_and_files_name_dict():
-    path = '/Users/axel/Documents/perso/repos/manga-downloader/chapters/'
-
-    ds_store_to_rm = Path(f"{path}.DS_Store")
+    ds_store_to_rm = Path(f"{PARENT_DIR}000/.DS_Store")
     if ds_store_to_rm.is_file():
-        os.remove(f"{path}.DS_Store")
+        os.remove(f"{PARENT_DIR}000/.DS_Store")
 
     folders_and_files_name = {}
-    for subdir, dirs, files in os.walk(path):
+    for subdir, dirs, files in os.walk(PARENT_DIR):
         for file in files:
             key, value = os.path.basename(subdir), file
-            folders_and_files_name.setdefault(key, []).append(value)
+            folders_and_files_name.setdefault(key, []).append(f"{subdir}/{value}")
 
-    for names in folders_and_files_name:
-        folders_and_files_name[names].sort()
+    sorted_dict = {k: folders_and_files_name[k] for k in sorted(folders_and_files_name)}
 
-    return folders_and_files_name
+    for value_ in sorted_dict.values():
+        value_.sort()
 
-
-def merge_images(folders_and_files_name):
-    # Map C-API to python
-    library.MagickAppendImages.argtypes = (ctypes.c_void_p, ctypes.c_bool)
-    library.MagickAppendImages.restype = ctypes.c_void_p
-    path = '/Users/axel/Documents/perso/repos/manga-downloader/chapters/0'
-    jpg_001 = '/Users/axel/Documents/perso/repos/manga-downloader/chapters/0/001.jpg'
-    jpg_002 = '/Users/axel/Documents/perso/repos/manga-downloader/chapters/0/002.jpg'
-
-    # Append all pages into one new image
-    new_ptr = library.MagickAppendImages(jpg_001, jpg_002, True)
-    library.MagickWriteImage(new_ptr, b'/Users/axel/Documents/perso/repos/manga-downloader/chapters/0/output.png')
-    library.DestroyMagickWand(new_ptr)
+    return sorted_dict
 
 
-def merge_images_2(folders_and_files_name):
-    path = '/Users/axel/Documents/perso/repos/manga-downloader/chapters/'
-    filenames_string_creation = []
-    for folder, filename in folders_and_files_name.items():
-        filenames_string_creation.append(filename[0])
-        pprint(filenames_string_creation)
-        bash = subprocess.run(
-            ["magick", "convert", "-append", "001.jpg", "002.jpg", "003.jpg", "004.jpg", "005.jpg", "chapter0.png"],
-            stderr=subprocess.PIPE,
-            text=True,
-            cwd=path + folder)
-        print(bash.stderr)
+def merge_images_in_one_png(folders_and_files_name):
+    chapters_path_list = []
+    for folder in folders_and_files_name:
+        print(f"Merging all JPGs into one PNG for chapter-{folder}...")
+        Image.MAX_IMAGE_PIXELS = None
+        images = [Image.open(x) for x in folders_and_files_name[folder]]
+        widths, heights = zip(*(i.size for i in images))
+
+        total_height = sum(heights)
+        max_width = max(widths)
+
+        new_im = Image.new('RGB', (max_width, total_height))
+
+        y_offset = 0
+        for im in images:
+            new_im.paste(im, (0, y_offset))
+            y_offset += im.size[1]
+
+        new_im.save(f'{PARENT_DIR}{folder}/chapter-{folder}.png')
+        chapters_path_list.append(f'{PARENT_DIR}{folder}/chapter-{folder}.png')
+        return chapters_path_list
+
+
+def compress_pngs(chapters_path_list):
+    for chapter_path in chapters_path_list:
+        remove_extension = os.path.splitext(chapter_path)[0]
+        take_only_number = remove_extension.rsplit('-', 3)
+        print(f"Compressing PNG for chapter-{take_only_number[1]}...")
+        Image.MAX_IMAGE_PIXELS = None
+        compressed_png = Image.open(chapter_path)
+        compressed_png = compressed_png.convert("P", palette=Image.ADAPTIVE, colors=256)
+        compressed_png.save(f'{PARENT_DIR}{take_only_number[1]}/chapter-{take_only_number[1]}.png', optimize=True)
