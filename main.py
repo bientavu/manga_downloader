0d458f079661218201273a934a159931364bfb4f
bientavu <dofus.jane@gmail.com>
2023-01-18 17:05:23 +0100

    Init of v2 app, need cleaning

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from app.download_images import get_chapters_urls, create_folders, count_number_of_dirs, \
-    curl_images_from_urls_with_selenium, curl_images_from_urls_without_selenium
+    curl_images_from_urls_with_selenium, curl_images_from_urls_without_selenium, \
+    get_chapters_for_custom_urls
 from app.merge_images import create_folders_and_files_name_dict, zip_chapters, \
     create_chapters_path_list, change_extension_to_cbz, rm_work_dir
-from variables_and_constants import CHAPTER_NUMBERS, INPUTS, SELECT_MANGA
+from variables_and_constants import CHAPTER_NUMBERS, INPUTS, SELECT_MANGA, SEASON_NUMBER, EPISODE_NUMBERS
 
 create_folders()
+# chapters_urls = get_chapters_for_custom_urls(CHAPTER_NUMBERS, SEASON_NUMBER, EPISODE_NUMBERS)
 chapters_urls = get_chapters_urls(CHAPTER_NUMBERS)
 full_paths = count_number_of_dirs()
 
